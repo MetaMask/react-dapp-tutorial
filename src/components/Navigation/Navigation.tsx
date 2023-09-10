a92d564c398245125b207be4f678dac49cfba477
httpjunkie <eric@httpjunkie.com>
2023-09-10 02:49:25 -0400

    add react-ui and provider to app

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,36 +1,18 @@
-import { useMetaMask } from '~/hooks/useMetaMask'
-import { formatAddress } from '~/utils'
+// import { useMetaMask } from '~/hooks/useMetaMask'
+import { MetaMaskButton } from '@metamask/sdk-react-ui'
+// import { formatAddress } from '~/utils'
 import styles from './Navigation.module.css'
 
 export const Navigation = () => {
 
-  const { wallet, hasProvider, isConnecting, connectMetaMask } = useMetaMask()
+  // const { wallet, hasProvider, isConnecting, connectMetaMask } = useMetaMask()
 
   return (
     <div className={styles.navigation}>
       <div className={styles.flexContainer}>
         <div className={styles.leftNav}>Vite + React & MetaMask</div>
         <div className={styles.rightNav}>
-          {!hasProvider &&
-            <a href="https://metamask.io" target="_blank" rel="noreferrer">
-              Install MetaMask
-            </a>
-          }
-          {window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&
-            <button disabled={isConnecting} onClick={connectMetaMask}>
-              Connect MetaMask
-            </button>
-          }
-          {hasProvider && wallet.accounts.length > 0 &&
-            <a
-              className="text_link tooltip-bottom"
-              href={`https://etherscan.io/address/${wallet}`}
-              target="_blank"
-              data-tooltip="Open in Block Explorer" rel="noreferrer"
-            >
-              {formatAddress(wallet.accounts[0])}
-            </a>
-          }
+          <MetaMaskButton theme={'light'} color="white"></MetaMaskButton>
         </div>
       </div>
     </div>
