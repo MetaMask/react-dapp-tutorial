88db1d962a07dbc6887aadeec85452e730954587
httpjunkie <eric@httpjunkie.com>
2023-09-11 13:11:06 -0500

    update sdk and options

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,18 @@ import { MetaMaskError } from './components/MetaMaskError'
 import { MetaMaskProvider } from '@metamask/sdk-react'
 
 export const App = () => {
-
+  const options = {
+    logging: {
+      developerMode: false,
+    },
+    checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
+    dappMetadata: {
+      name: 'Demo React App',
+      url: window.location.host,
+    },
+  }
   return (
-    <MetaMaskProvider debug={false} sdkOptions={{
-      logging: {
-        developerMode: false,
-      },
-      checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
-      dappMetadata: {
-        name: 'Demo React App',
-        url: window.location.host,
-      },
-    }}>
+    <MetaMaskProvider debug={false} sdkOptions={options}>
       <div className={styles.appContainer}>
         <Navigation />
         <Display />
